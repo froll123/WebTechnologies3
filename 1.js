31064906c224660e64a2d15f5d7e685dafbc0648
froll_123@mail.ru <froll_123@mail.ru>
2023-05-30 16:09:52 +0300

    Added tasks for the 3rd seminar

---
+++ 1.js
@@ -0,0 +1,7 @@
+const a = Number.parseFloat(prompt("Введите температуру в градусах Цельсия"));
+
+function convert(x) {
+    return ((9 / 5) * x + 32);
+}
+
+alert(`Температура по Цельсию = ${a.toFixed(1)}, температура по Фаренгейту = ${convert(a).toFixed(1)}`);
